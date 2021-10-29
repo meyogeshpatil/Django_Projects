ed045a0565fbadfc109e25ded9f80d273f31a640
yogesh <mepatilyogesh@gmail.com>
2021-10-29 10:32:19 +0530

    skipped staging area and fixed

--- fact.py
+++ fact.py
@@ -1,4 +1,9 @@
+import time
 def fact(n):
     return 1 if n==0 else n*fact(n-1)
+
+time.slee[(3)]
 n=int(input('Please enter the number: '))
+time.sleep(3)
+print('This function really helps to understands how recursion works in python')
 print( 'factorial of {} is:{}'.format(n,fact(n)))
